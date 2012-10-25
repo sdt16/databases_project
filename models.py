0222b738669eba8e4110f8bcf9e6e5eb5bc0ac64
Schuyler Thompson <skooz@skooz.net>
2012-10-25 15:48:55 -0400

    added __init__ and person table

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     vendor_code = db.Column(db.String, db.ForeignKey('vendor_code.vendor_code'), nullable=False)
-    vendor_code_relation = db.relationship('VendorCode', backref=db.backref('users', lazy='dynamic'))
+    #vendor_code_relation = db.relationship('VendorCode', backref=db.backref('users', lazy='dynamic'))
 
     def __init__(username, message, email, password, vendor_code):
         self.username = username
@@ -27,6 +27,8 @@ class User(db.Model):
 
 class Book(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
+	vendor_code = db.Column(db.String, db.ForeignKey('vendor_code.vendor_code'), nullable=False)
+	#vendor_code_relation = db.relationship('VendorCode', backref=db.backref('users', lazy='dynamic'))
 	title = db.Column(db.String)
 	imprint = db.Column(db.String)
 	eisbn = db.Column(db.String)
@@ -39,8 +41,40 @@ class Book(db.Model):
 	desciption = db.Column(db.String)
 	bisac = db.Column(db.String)
 	bic = db.Column(db.String)
-	territory = db.Column(db.Enum(US, GB, FR, IT, CN, JP, ES, IE, DE))
+	territory = db.Column(db.Enum(US, GB, FR, IT, CN, JP, ES, IE, DE, name='country_codes'))
 	adult = db.Column(db.Boolean)
 	edition = db.Column(db.String)
 	series_title = db.Column(db.String)
 	volume = db.Column(db.String)
+
+
+	def __init__(title, imprint, eisbn, pisbn, language, list_price, currency, release_date, publishing_date, desciption, 
+		bisac, bic, territory, adult, edition, series_title, volume):
+		self.title = title
+		self.imprint = imprint
+		self.eisbn = eisbn
+		self.pisbn = pisbn
+		self.language = language
+		self.list_price = list_price
+		self.currency = currency
+		self.release_date = release_date
+		self.publishing_date = publishing_date
+		self.desciption = desciption
+		self.bisac = bisac
+		self.bic = bic
+		self.territory = territory 
+		self.adult = adult
+		self.edition = edition
+		self.series_title = series_title
+		self.volume = volume
+
+class Person(db.Model):
+	id = (db.Column(db.Integer, primary_key=true))
+	first_name = db.Column(db.String, nullable = False)
+	last_name = db.Column(db.String, nullable = False)
+	birthday = db.Column(db.DateTime)
+
+	def __init__(first_name, last_name, birthday):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.birthday = birthday
